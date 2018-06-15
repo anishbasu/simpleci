45542919c591e9b51ffe816aad80eace091d95f8
Anish Basu <anishbasu.pr@gmail.com>
2018-06-15 18:01:06 -0400

    Fixed parts correspondinng to termux

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-const express = require('require')
-const bodyParser = require('bodyparser')
-const ngrok = require('ngrok')
+const express = require('express')
+const bodyParser = require('body-parser')
 const termux = require('termux')
-(async function() {
-	const url = await ngrok.connect({port: 8080, configFile: '~/.ngrok2/ngrok.yml'})
-	console.log(`CI available at ${url}`)
-})();
+
 //Make App Variable
 const app = express()
 //Middlewares
 app.use(bodyParser.json())
 //Hook
-app.get('/gh_hook', (req, res) => {
+app.post('/gh_hook', (req, res) => {
 	console.log(req.body)
 	res.sendStatus(200)
-}
+})
 //Listen
 app.listen(8080, () => {
 	console.log(`CI Server Started on port 8080`)
 })
 
-
-process.on('SIGTERM', shutDown)
-process.on('SIGINT', shutDown)
-
-const shutDown = () => {
-	ngrok.disconnect()
-}
